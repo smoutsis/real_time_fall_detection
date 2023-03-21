bedf2d962d50ea9698bb97749d637ca20c8deab9
smoutsis <79532335+smoutsis@users.noreply.github.com>
2023-03-21 16:44:27 +0200

    Add files via upload

---
+++ target_dataset/unzip.py
@@ -0,0 +1,50 @@
+import os
+import shutil
+import zipfile
+
+'''This script will unzip the file and will put all the videos in the LE2I folder that is created. The names of the videos correspond to the file labels.csv'''
+
+path = 'path where the FallDataset.zip is' # download the dataset from https://imvia.u-bourgogne.fr/en/database/fall-detection-dataset-2.html
+zipfil = 'FallDataset.zip'
+
+path_un  = os.path.join(path,zipfil)
+
+with zipfile.ZipFile(path_un, 'r') as zip_ref:
+    zip_ref.extractall(path)
+
+
+folders = ['Coffee_room_01', 'Coffee_room_02', 'Home_01', 'Home_02', 'Lecture_room', 'Office']
+
+for folder in folders:
+    
+    # os.system('mkdir '+ os.path.join(path,folder))
+    
+    with zipfile.ZipFile(os.path.join(path,folder)+'.zip', 'r') as zip_ref:
+        zip_ref.extractall(path)
+
+os.rename(os.path.join(path,'Lecture room'), os.path.join(path,'Lecture_room'))
+   
+os.system('mkdir ' + path + '/LE2I')   
+     
+for folder in folders:
+    
+    files = os.listdir(os.path.join(path, folder))
+    
+    if len(files) == 2:
+        files2 = os.listdir(os.path.join(path, folder, files[0]))
+        for f in files2:
+            src = os.path.join(path, folder, files[0], f) 
+            dst = os.path.join(path, 'LE2I', folder+'_'+f) 
+            
+            shutil.move(src, dst)
+        
+    else:
+        for f in files:
+            src = os.path.join(path, folder, f) 
+            dst = os.path.join(path, 'LE2I', folder+'_'+f) 
+            
+            shutil.move(src, dst)
+    
+    
+    
+
